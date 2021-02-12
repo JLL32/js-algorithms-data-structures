326f4b0da780cb6eaa62198a518aa105c7517167
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-02-12 11:13:24 +0100

    adds insert() to BinarySearchTree

--- src/data-structures/trees/binary-search-tree.ts
+++ src/data-structures/trees/binary-search-tree.ts
@@ -8,4 +8,29 @@ class BNode<T> {
 
 class BinarySearchTree<T> {
     constructor(public root: BNode<T> | null = null) {}
+
+    insert(value: T): BNode<T> {
+        let newNode = new BNode(value);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return this.root;
+        }
+
+        let tempNode = this.root;
+        for (;;) {
+            if (value > tempNode.value) {
+                if (tempNode.right == null) {
+                    tempNode.right = newNode;
+                    return newNode;
+                } else tempNode = tempNode.right;
+            }
+            if (value < tempNode.value) {
+                if (tempNode.left == null) {
+                    tempNode.left = newNode;
+                    return newNode;
+                } else tempNode = tempNode.left;
+            }
+        }
+    }
 }
