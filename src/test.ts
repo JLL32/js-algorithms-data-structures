0363dce1d47d4b072d4495150e9ddf69f959b902
JLLx86 <eabduljalil@outlook.com>
2021-02-14 19:47:53 +0100

    makes myBT immutable

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,13 @@
 import BST from "./data-structures/trees/binary-search-tree.ts";
 
 const TestBST = function () {
-    let myBT = BST<number>();
+    const myBT = BST<number>();
     console.log(myBT.insert(9));
     console.log(myBT.insert(4));
     console.log(myBT.insert(20));
     console.log(myBT.insert(1));
     console.log(myBT.insert(5));
-    console.log(JSON.stringify(myBT.root, null, "    "));
+    console.log(JSON.stringify(myBT.root, null, "  "));
 	console.log(typeof myBT.insert);
 };
 
